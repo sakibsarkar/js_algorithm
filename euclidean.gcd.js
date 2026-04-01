72fda652ad2fb188acd0a7db883febb7fd8f87ab
Nazmul Islam Sakib <sakibsarkar707@gmail.com>
2026-04-01 21:45:43 +0600

    feat: Euclidean gcd algorithm

---
+++ euclidean.gcd.js
@@ -0,0 +1,19 @@
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+};
+
+const multipleNumberGcd = (...nums) => {
+  let gcdResult = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+    gcdResult = gcd(num, gcdResult);
+  }
+  return gcdResult;
+};
+
+console.time("gcd");
+console.log(multipleNumberGcd(12, 20, 120, 48, 40, 72));
+console.timeEnd("gcd");
