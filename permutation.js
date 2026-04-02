a8858490553112cc7e345c0b4eabdd9b6e4cc717
Nazmul Islam Sakib <sakibsarkar707@gmail.com>
2026-04-02 22:50:34 +0600

    fix: permutation forumula fix

--- permutation.js
+++ permutation.js
@@ -4,15 +4,16 @@ const factorial = (numb) => {
 };
 
 /**
- * Calculate the permutation of n items taken r at a time
- * @param {number} n the number of items to choose from
+ * Calculate the number of permutations of n items taken r at a time
+ * @param {number} n the total number of items
  * @param {number} r the number of items to choose
- * @returns {number} the permutation of n items taken r at a time
- * The permutation of n items taken r at a time is the number of ways to arrange r items from a set of n items
- * It is calculated as n! / r!
+ * @returns {number} the number of permutations
+ * The number of permutations of n items taken r at a time is
+ * the factorial of n divided by the factorial of n - r
  */
 const permutation = (n, r) => {
-  return factorial(n) / factorial(r);
+  return factorial(n) / factorial(n - r);
 };
-
-console.log(permutation(6, 2));
+console.time("permutation");
+console.log(permutation(6, 4));
+console.timeEnd("permutation");
